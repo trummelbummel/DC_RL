4b685b774e17753773a1cc8419188c8a08e19f26
Theresa Fruhwuerth <theresa.fruhwuerth@gmail.com>
2018-08-24 15:37:03 +0200

    Update q_learning.py

--- gym_dc/gym_dc/envs/q_learning.py
+++ gym_dc/gym_dc/envs/q_learning.py
@@ -2,8 +2,9 @@ from keras.layers import Dense
 from keras.models import Sequential
 from keras.optimizers import Adam
 import numpy as np
-from numpy import random
+from random import sample
 from collections import deque
+
 # Q learning implementation
 # Initialize Q(s,a) arbitrarily
 # Repeat (for each generation):
@@ -14,7 +15,7 @@ from collections import deque
 # 		Q(s,a) += alpha * (r + gamma * max,Q(s') - Q(s,a))
 # 		s = s'
 #
-
+# TODO: doesn't seem to train right now.
 # 1. receive state and feed to NN and network that decides best action to execute
 # 2. network trained millions of times via Q learning to maximize future expected reward
 
@@ -35,6 +36,9 @@ class DeepQNetwork:
         self.memory = deque(maxlen=2000)
         self.model = self._buildmodel()
         self.env = env
+        self.time = self.env.time
+        self.action_indizes = {}
+        self.max_ind = 0
 
     def fit_NN(self, state, reward_value):
         ''' fit reward value from a certain state after carrying out an action decreases gap between prediction to target according to learning rate'''
@@ -56,18 +60,33 @@ class DeepQNetwork:
 
     def replay(self, batch_size):
         ''' experience replay based on old experiences '''
-        # print('memory')
-        # print(self.memory)
-        minibatch = random.sample(self.memory, batch_size)
+        minibatch = sample(self.memory, batch_size)
         for state, action, reward, nextstate, done in minibatch:
             target = reward
             if not done:
+                nextstate = np.reshape(nextstate, [1,nextstate.shape[0]])
+                # reward + distcounted future reward
                 target = reward + self.gamma * np.amax(self.model.predict(nextstate)[0])
             # make agent approximately map current state to future discounted reward
             target_f = self.model.predict(state)
-            target_f[0][action] = target
+            # reset target value for action to target
+            #action_index = self.get_action_index(action)
+            # print('target predicted')
+            # print(target_f)
+            # target_f[0][action_index] = target
             # train NN with state and approximate reward
             self.model.fit(state, target_f, epochs=1, verbose=0)
+
+    def get_action_index(self, action):
+        if str(action) in self.action_indizes.keys():
+            index = self.action_indizes.get(str(action))
+            return index
+        else:
+            self.max_ind += 1
+            index = self.max_ind
+            self.action_indizes[str(action)] = index
+            return index
+
 
     def act(self, state):
         ''' randomly select action at first by certain percentage corresponding to epsilon '''
@@ -82,7 +101,7 @@ class DeepQNetwork:
         act_values = self.model.predict(state)
         # pick action based on maximum reward
         # print('predicted action')
-        print(act_values)
+        #print(act_values)
         return act_values[0]
 
     def _buildmodel(self):
@@ -97,3 +116,14 @@ class DeepQNetwork:
        #
        NN.compile(loss='mse', optimizer=Adam(lr=self.learningrate))
        return NN
+
+    def get_stats_training(self):
+        ''' function to examine pogress  '''
+        first_weights = self.model.layers[0].get_weights()[0]
+        first_bias = self.model.layers[0].get_weights()[1]
+        second_weights = self.model.layers[1].get_weights()[0]
+        second_bias = self.model.layers[1].get_weights()[1]
+        print('variance weights first layer  {}'.format(np.var(first_weights)))
+        print('mean weights first layer  {}'.format(np.mean(first_weights)))
+        print('variance weights second layer  {}'.format(np.var(first_weights)))
+        print('mean weights second layer  {}'.format(np.mean(first_weights)))
